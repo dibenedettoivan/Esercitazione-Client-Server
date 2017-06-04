e4f92f104f0c4c0f6cf4278c7cbf3199d99ce125
dibenedettoivan <dibenedettoivan@gmail.com>
2017-06-04 10:29:47 +0200

    Modifica file

--- provaEq.py
+++ provaEq.py
@@ -4,6 +4,9 @@ a = input("Digita il numero: ")
 b = input("Digita il numero: ")
 c = input("Digita il numero: ")
 
+
+
+
 radice = math.sqrt(b**2 - (4*a*c))
 soluzione = (-b- radice)/2*a
 soluzione1 = (-b + radice)/2*a
